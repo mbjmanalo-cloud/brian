f103052a4814d3edfbe64874a028f1a79c102ca2
biji <mbjmanalo@tip.edu.ph>
2026-04-08 09:14:27 +0800

    fix firebase syntax

--- src/firebase.js
+++ src/firebase.js
@@ -2,28 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyAz5iW5ll9wHPR11AHVIiivwZ_aq2CEGBE",
-
   authDomain: "note-app-e5388.firebaseapp.com",
-
   projectId: "note-app-e5388",
-
   storageBucket: "note-app-e5388.firebasestorage.app",
-
   messagingSenderId: "850826817201",
-
   appId: "1:850826817201:web:cc235080b718737b2bfc90",
-
   measurementId: "G-QRMZ8L998Q"
-
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-<<<<<<< HEAD
-export { db };      
-=======
 export { db };
->>>>>>> dd3d88eda827ad1d9693b2933fc368bdb6e3a2cc
